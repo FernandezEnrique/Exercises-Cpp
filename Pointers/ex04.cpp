85910f7d2e81b630cb9f25b46601cf23aee71b62
Enrique <fernandezvelascoenrique@gmail.com>
2023-02-23 18:31:15 +0100

    Fixed Pointers/04

--- Pointers/ex04.cpp
+++ Pointers/ex04.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 
 int main () {
-    int v[50] = {1,3,5,7,9,11,13,15}; // Σ(2n - 1) = n² (maths)
-    int length = 8;
+    int v[50] = {1,3,5,7,9,11,13,15,17,19,21}; // Σ(2n - 1) = n² (maths)
+    int length = 11;
     int * ptr1, *ptr2;
     int sum = 0;
     ptr1 = &(v[0]);
     ptr2 = &(v[length-1]);
     while (ptr1 <= ptr2) {
         sum += *ptr1;
-        sum += *ptr2;
+        if (ptr1 != ptr2)
+            sum += *ptr2;
         ptr1++;
         ptr2--;
     }
